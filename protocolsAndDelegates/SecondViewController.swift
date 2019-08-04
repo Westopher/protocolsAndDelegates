7108ad00053d66ecc4257c64d8a2c860fe2bf810
West Kraemer <kraemerwest@gmail.com>
2019-08-04 16:56:53 +0200

    create iboutlets and actions for UI elements

--- protocolsAndDelegates/SecondViewController.swift
+++ protocolsAndDelegates/SecondViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var label2: UILabel!
+    
+    @IBOutlet weak var textField2: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +21,8 @@ class SecondViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func sendDataBack(_ sender: Any) {
     }
-    */
+    
 
 }
